5b8db6e49a3df6f93d1485d4f5c3d8c1ccb2ebea
Alec Leamas <leamas.alec@nowhere.net>
2013-03-19 05:15:00 +0100

    __init__: update metadata

--- __init__.py
+++ __init__.py
@@ -37,25 +37,20 @@ Listen for data on a TCP port and forward to designated channel(s).
 import supybot
 import supybot.world as world
 
-# Use this for the version of this plugin.  You may wish to put a CVS keyword
-# in here if you're keeping the plugin in CVS or some similar system.
-__version__ = ""
+__version__ = "0"
 
-# XXX Replace this with an appropriate author or supybot.Author instance.
-__author__ = supybot.authors.unknown
+__author__ = "Alec Leamas"
 
 # This is a dictionary mapping supybot.Author instances to lists of
 # contributions.
 __contributors__ = {}
 
 # This is a url where the most recent plugin package can be downloaded.
-__url__ = ''    # 'http://supybot.com/Members/yourname/Irccat/download'
+__url__ = 'https://github.com/leamas/supybot-irccat'
 
 import config
 import plugin
 reload(plugin)    # In case we're being reloaded.
-# Add more reloads here if you add third-party modules and want them to be
-# reloaded when this plugin is reloaded.  Don't forget to import them as well!
 
 if world.testing:
     import test
